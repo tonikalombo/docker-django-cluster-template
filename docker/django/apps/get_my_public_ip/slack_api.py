d34eadcee513dac86f546a1310ae8905bf4fa66f
Toni Yannick Kalombo <toni@kalombo.me>
2019-08-18 17:14:56 +0200

    removing inadvertsntly hardcoded secrets in slack_api.py

--- docker/django/apps/get_my_public_ip/slack_api.py
+++ docker/django/apps/get_my_public_ip/slack_api.py
@@ -4,8 +4,10 @@ import json
 
 class SlackApi():
         def post_message_to_slack(payload):
-                slack_url = "https://hooks.slack.com/services/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"
-                #slack_url = "https://hooks.slack.com/services/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"
+        		slack_domain = "XXXXX"
+        		slack_service_id = "ZZZZZ"
+        		slack_service_token = "YYYYYYY"
+                slack_url = "https://hooks.slack.com/services/"+slack_domain+"/"+slack_service_id+"/"+slack_service_token    
                 headers = {'Content-type':'application/json', }
                 slack_response = requests.post(slack_url, headers=headers, data=payload)
                 #custom_response = Response( status=slack_response.status_code, content_type="application/json",headers={'X-Slack-No-Retry': 1})        
